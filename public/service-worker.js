e75218e6b2da344e10d8fd84a05e3903a6a8f550
shuting <zst9394@outlook.com>
2021-04-24 18:09:16 -0400

    change if (event.request.url.includes(/api)) {

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const FILES_TO_CACHE = [
     "/",
+    "/db.js",
     "/index.html",
     "/styles.css",
-    "/db.js",
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
